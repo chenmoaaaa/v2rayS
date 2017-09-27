acbef9911ba2ce51c232633aba61aa6891f58b79
rkzrdm <tinyadapter@yandex.com>
2017-09-27 16:46:18 +0800

    add pac file cache

--- v2rayS/Utils/HttpServer.cs
+++ v2rayS/Utils/HttpServer.cs
@@ -4,50 +4,81 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using v2rayS.Controllers;
 
 namespace v2rayS.Utils
 {
     class HttpServer
     {
         public static HttpListener Listener = new HttpListener();
+        public static Task CurrentTask;
+        public static string PacString = File.ReadAllText(Process.CurrentDirectory + "\\pac");
+        private static CancellationTokenSource cts;
+        private static CancellationToken ct;
+        private static PacWatcher _pacWatcher;
+
+        private static Action action = () =>
+        {
+            while (true)
+            {
+                //等待请求连接
+                //没有请求则GetContext处于阻塞状态
+                try
+                {
+                    HttpListenerContext ctx = Listener.GetContext();
+                    ctx.Response.StatusCode = 200;//设置返回给客服端http状态代码
+                    string name = ctx.Request.QueryString["name"];
+
+                    if (name != null)
+                    {
+                        Console.WriteLine(name);
+                    }
+
+
+                    //使用Writer输出http响应代码
+                    using (StreamWriter writer = new StreamWriter(ctx.Response.OutputStream))
+                    {
+                        ctx.Response.ContentType = "application/x-ns-proxy-autoconfig";
+                        writer.WriteLine(PacString);
+                        writer.Close();
+                        ctx.Response.Close();
+                    }
+                }
+                catch (Exception) { }
+            }
+        };
 
         public static void StartListener()
         {
+            //初始化Cancellation
+            cts = new CancellationTokenSource();
+            ct = cts.Token;
+
             Listener.AuthenticationSchemes = AuthenticationSchemes.Anonymous;//指定身份验证 Anonymous匿名访问
             Listener.Prefixes.Add("http://127.0.0.1:1081/pac/");
 
             Listener.Start();
-            new Task(() =>
+            CurrentTask = Task.Factory.StartNew(action, ct);
+
+            _pacWatcher = new PacWatcher(Process.CurrentDirectory, "pac");
+        }
+
+        public static void Refresh()
+        {
+            cts.Cancel();
+            while(true)
             {
-                while (true)
+                try
                 {
-                    //等待请求连接
-                    //没有请求则GetContext处于阻塞状态
-                    try
-                    {
-                        HttpListenerContext ctx = Listener.GetContext();
-                        ctx.Response.StatusCode = 200;//设置返回给客服端http状态代码
-                        string name = ctx.Request.QueryString["name"];
-
-                        if (name != null)
-                        {
-                            Console.WriteLine(name);
-                        }
-
-
-                        //使用Writer输出http响应代码
-                        using (StreamWriter writer = new StreamWriter(ctx.Response.OutputStream))
-                        {
-                            ctx.Response.ContentType = "application/x-ns-proxy-autoconfig";
-                            writer.WriteLine(File.ReadAllText(Directory.GetCurrentDirectory() + "\\pac"));
-                            writer.Close();
-                            ctx.Response.Close();
-                        }
-                    }
-                    catch (Exception) {}
+                    PacString = File.ReadAllText(Process.CurrentDirectory + "\\pac");
+                    break;
                 }
-            }).Start();
+                catch (Exception) { }
+            }
+
+            CurrentTask = Task.Factory.StartNew(action, ct);
         }
     }
 }
---
+++ v2rayS/Utils/PacWatcher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace v2rayS.Utils
+{
+    class PacWatcher
+    {
+        public PacWatcher(string path, string filter)
+        {
+            this.WatcherStrat(path, filter);
+        }
+
+        private void WatcherStrat(string path, string filter)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher
+            {
+                Path = path,
+                Filter = filter
+            };
+
+            watcher.Changed += (sender, e) =>
+            {
+                HttpServer.Refresh();
+            };
+
+            watcher.EnableRaisingEvents = true;
+        }
+    }
+}
